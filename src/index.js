06e62ca1379d4e2e38bc86d143800cb33b216c86
ikari-shirei <algrsyu@protonmail.com>
2021-06-28 08:29:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import '/src/style/style.scss';
+import '/src/modules/svg-imports.js';
+
+import '/src/modules/navbar.js';
+
+import '/src/modules/new-task.js';
+import '/src/modules/task-create-DOM.js';
+import '/src/modules/projects.js';
