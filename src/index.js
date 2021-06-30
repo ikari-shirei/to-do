0572c07c30d326984a03bea872181ff7b4149963
ikari-shirei <algrsyu@protonmail.com>
2021-06-30 19:44:44 +0300

    Combine project and task modules

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import '/src/modules/svg-imports.js';
 
 import '/src/modules/navbar.js';
 
+/* 
 import '/src/modules/new-task.js';
 import '/src/modules/task-create-DOM.js';
-import '/src/modules/projects.js';
+import '/src/modules/projects.js'; */
+
+import '/src/modules/projects-tasks.js';
