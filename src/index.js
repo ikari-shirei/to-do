3c941d4b41238961a666dbc39b84be9b98f578c5
ikari-shirei <algrsyu@protonmail.com>
2021-07-04 14:28:24 +0300

    Import related files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
 import '/src/style/style.scss';
 import '/src/modules/svg-imports.js';
-
 import '/src/modules/navbar.js';
-
-/* 
-import '/src/modules/new-task.js';
-import '/src/modules/task-create-DOM.js';
-import '/src/modules/projects.js'; */
-
 import '/src/modules/projects-tasks.js';
